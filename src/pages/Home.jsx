70c46325006cf8964037545da100cc22566e246f
pttkachuk <petrotkachuk6@gmail.com>
2023-08-01 15:34:10 +0200

    styled home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import {
+  HomeDescr,
+  HomeTitle,
+  StyledHomeDiv,
+} from './StyledPages/StyledHomePage';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <StyledHomeDiv>
+      <div>
+        <HomeTitle>Hi there 👋🏻</HomeTitle>
+        <HomeDescr>
+          Welcome to Phonebook app. <br />
+          Here you can create your personal account and manage your contacts.
+          For your privacy we made protect Contacts page so unwanted guests will
+          not be able to see your personal data. <br />
+        </HomeDescr>
+      </div>
+    </StyledHomeDiv>
+  );
 };
 
 export default Home;
---
+++ src/pages/StyledPages/StyledHomePage.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const StyledHomeDiv = styled.div`
+ display: flex;
+  justify-content: space-evenly;
+  padding: 40px;
+`;
+
+export const HomeTitle = styled.h2`
+text-align: center;
+font-size: 32px;
+font-weight: 700;
+margin-bottom: 60px;
+`;
+
+export const HomeDescr = styled.p`
+margin: 0;
+padding: 0;
+max-width: 420px;
+font-size: 25px;
+font-weight: 400;
+text-align:center;
+`;
