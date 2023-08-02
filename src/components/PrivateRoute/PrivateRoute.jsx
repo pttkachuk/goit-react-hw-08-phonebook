96b1bf35e1c91bf3618dcfbfd0fad4eb55cd7dc1
pttkachuk <petrotkachuk6@gmail.com>
2023-08-02 08:40:15 +0200

    add missing propTypes

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,7 +1,12 @@
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 export const PrivateRoute = ({ children }) => {
   const token = useSelector(state => state.user.token);
   return token ? children : <Navigate to="/login" />;
+};
+
+PrivateRoute.propTypes = {
+  children: PropTypes.node,
 };
--- src/components/PublicRoute/PublicRoute.jsx
+++ src/components/PublicRoute/PublicRoute.jsx
@@ -1,7 +1,12 @@
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 export const PublicRoute = ({ children }) => {
   const token = useSelector(state => state.user.token);
   return !token ? children : <Navigate to="/contacts" />;
+};
+
+PublicRoute.propTypes = {
+  children: PropTypes.node,
 };
